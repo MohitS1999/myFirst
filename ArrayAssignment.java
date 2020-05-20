67ff2554e91489d4337059c8f0e5e806417f255c
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-05-20 11:05:27 +0530

    Basic Operations

--- ArrayAssignment.java
+++ ArrayAssignment.java
@@ -1,5 +1,6 @@
 import java.util.*;
 class ArrayAssignment{
+	//Gives the max value of Array
 	static int getmax(int []a){
 		int max=a[0];
 		for(int i=0;i<a.length;i++){
@@ -8,6 +9,7 @@ class ArrayAssignment{
 			}
 			}return max;
 	}
+	//Gives the Min value of Array
 	static int getmin(int []a1){
 		int min=a1[0];
 		for(int i=0;i<a1.length;i++){
@@ -16,6 +18,7 @@ class ArrayAssignment{
 			}
 			}return min;
 	}
+	//Gives the Max Value of Matrix
 	static int getMax(int [][]b){
 		int Max=b[0][0];
 		for(int i=0;i<b.length;i++){
@@ -26,6 +29,7 @@ class ArrayAssignment{
 			}
 		}return Max;
 	}
+	//Gives the Min value of Matrix
 	static int getMin(int [][]b1){
 		int Min=b1[0][0];
 		for(int i=0;i<b1.length;i++){
@@ -36,6 +40,7 @@ class ArrayAssignment{
 			}
 		}return Min;
 	}
+	//Sort the Array
 	void sort(int []c){
 		int temp=0;
 		int m=0;
@@ -55,6 +60,7 @@ class ArrayAssignment{
 		System.out.println("  ");
 		}
 	}
+	//Sort the Matrix
 	void sort2(int [][]d){
 		Arrays.sort(d);
 		for(int i=0;i<d.length;i++){
@@ -64,6 +70,7 @@ class ArrayAssignment{
 		}
 		}
 	}
+	//Union of two array
 	void union(int x1[],int y1[]){
 		int count=0;
 		int w[]=new int[x1.length+y1.length];
@@ -88,6 +95,7 @@ class ArrayAssignment{
 		System.out.println(temp[j]+"  ");
 	}
 	}
+	//Intersection of Two Array
 	void intersection(int p[],int q[]){
 		int cc=0;
 		int m[]=new int[p.length+q.length];
@@ -112,6 +120,7 @@ class ArrayAssignment{
 			System.out.println(temp[k]+"  ");
 		}
 	}
+	//Main function
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		printaction();
@@ -121,132 +130,114 @@ class ArrayAssignment{
 		int ch=s.nextInt();
 		s.nextLine();
 		switch(ch){
-			case 1:int []a;
-						System.out.println("enter the size of an array");
-						int x3=s.nextInt();
-						a=new int[x3];
-						System.out.println("Enter the values of an Array");
-						for(int i=0;i<a.length;i++){
-							a[i]=s.nextInt();
+			//For Max Array function
+			case 1: int []a;
+				System.out.println("enter the size of an array");
+				int x3=s.nextInt();
+				a=new int[x3];
+				System.out.println("Enter the values of an Array");
+				for(int i=0;i<a.length;i++){
+					a[i]=s.nextInt();
+				}
+				int max=getmax(a);
+				System.out.println("MAXIMUM VALUE:-  "+max);
+				break;
+			//for Min Array Function
+			case 2: int []a1;
+				System.out.println("enter the size of an array");
+				int x4=s.nextInt();
+				a1=new int[x4];
+				System.out.println("Enter the values of an Array");
+				for(int i=0;i<a1.length;i++){
+					a1[i]=s.nextInt();
+				}
+				int min=getmin(a1);
+				System.out.println("MINIMUM VALUEs:-  "+min);
+				break;	
+			//for Max MAtrix function
+			case 3: int b[][];
+				System.out.println("Enter row of an arrray");
+				int y3=s.nextInt();
+				System.out.println("Enter the column of an Array");
+				int z3=s.nextInt();
+				b=new int[y3][z3];
+				System.out.println("Enter the values of an 2d Array");
+				for(int i=0;i<b.length;i++){
+					for(int j=0;j<b.length;j++){
+							b[i][j]=s.nextInt();
 						}
-						int max=getmax(a);
-						System.out.println("MAXIMUM VALUE:-  "+max);
-						break;
-			case 2:int []a1;
-						System.out.println("enter the size of an array");
-						int x4=s.nextInt();
-						a1=new int[x4];
-						System.out.println("Enter the values of an Array");
-						for(int i=0;i<a1.length;i++){
-							a1[i]=s.nextInt();
-						}
-						int min=getmin(a1);
-						System.out.println("MINIMUM VALUEs:-  "+min);
-						break;			
-			case 3:int b[][];
-						System.out.println("Enter row of an arrray");
-						int y3=s.nextInt();
-						System.out.println("Enter the column of an Array");
-						int z3=s.nextInt();
-						b=new int[y3][z3];
-						System.out.println("Enter the values of an 2d Array");
-						for(int i=0;i<b.length;i++){
-							for(int j=0;j<b.length;j++){
-								b[i][j]=s.nextInt();
-								}
-							}
-						int Max=getMax(b);
-						System.out.println("Max value of 2D Array:-  "+Max);
-						break;
-			case 4:int b1[][];
-						System.out.println("Enter row of an arrray");
-						int y=s.nextInt();
-						System.out.println("Enter the column of an Array");
-						int z=s.nextInt();
-						b1=new int[y][z];
-						System.out.println("Enter the values of an 2d Array");
-						for(int i=0;i<b1.length;i++){
-							for(int j=0;j<b1.length;j++){
-								b1[i][j]=s.nextInt();
-								}
-							}
-						int Min=getMin(b1);	
-						System.out.println("MIn value of 2D Array:-  "+Min);
-						break;
-			case 5:int []a2;
-						System.out.println("enter the size of an array");
-						int x=s.nextInt();
-						a2=new int[x];
-						System.out.println("Enter the values of an Array");
-						for(int i=0;i<a2.length;i++){
-							a2[i]=s.nextInt();
-						}
-						ArrayAssignment ab=new ArrayAssignment();
-						ab.sort(a2);
-						break;
-			case 6:int b4[][];
-						System.out.println("Enter row of an arrray");
-						int y5=s.nextInt();
-						System.out.println("Enter the column of an Array");
-						int z5=s.nextInt();
-						b4=new int[y5][z5];
-						System.out.println("Enter the values of an 2d Array");
-						for(int i=0;i<b4.length;i++){
-							for(int j=0;j<b4.length;j++){
-								b4[i][j]=s.nextInt();
-								}
-							}
-						ArrayAssignment ab1=new ArrayAssignment();
-						ab1.sort2(b4);
-						break;
-			case 7:int x1[];
-			       int y1[];
-				   System.out.println("Enter the size of first Array");
-				   int x2=s.nextInt();
-				   x1=new int[x2];
-				   System.out.println("ENTER the size of second Array");
-				   int y2=s.nextInt();
-				   y1=new int[y2];
-				   System.out.println("Enter the elements of an first Array");
-				   for(int i=0;i<x1.length;i++){
-					   x1[i]=s.nextInt();
-				   }
-				   for(int j=0;j<y1.length;j++){
-					   y1[j]=s.nextInt();
-				   }
-				   break;
-			case 9:printaction();
-					break;
-			case 10:System.out.println("Thnku for using");
-					quit=false;
-					break;
+					}
+				int Max=getMax(b);
+				System.out.println("Max value of 2D Array:-  "+Max);
+				break;
+			//for min matrix function
+			case 4: int b1[][];
+				System.out.println("Enter row of an arrray");
+				int y=s.nextInt();
+				System.out.println("Enter the column of an Array");
+				int z=s.nextInt();
+				b1=new int[y][z];
+				System.out.println("Enter the values of an 2d Array");
+				for(int i=0;i<b1.length;i++){
+					for(int j=0;j<b1.length;j++){
+						b1[i][j]=s.nextInt();
+					}
+				}
+				int Min=getMin(b1);	
+				System.out.println("MIn value of 2D Array:-  "+Min);
+				break;
+			//Sort an array
+			case 5: int []a2;
+				System.out.println("enter the size of an array");
+				int x=s.nextInt();
+				a2=new int[x];
+				System.out.println("Enter the values of an Array");
+				for(int i=0;i<a2.length;i++){
+					a2[i]=s.nextInt();
+				}
+				ArrayAssignment ab=new ArrayAssignment();
+				ab.sort(a2);
+				break;
+			//sort a matrix function
+			case 6: int b4[][];
+				System.out.println("Enter row of an arrray");
+				int y5=s.nextInt();
+				System.out.println("Enter the column of an Array");
+				int z5=s.nextInt();
+				b4=new int[y5][z5];
+				System.out.println("Enter the values of an 2d Array");
+				for(int i=0;i<b4.length;i++){
+					for(int j=0;j<b4.length;j++){
+						b4[i][j]=s.nextInt();
+					}
+				}
+				ArrayAssignment ab1=new ArrayAssignment();
+				ab1.sort2(b4);
+				break;
+			//for print the options
+			case 9: printaction();
+				break;
+			case 10: System.out.println("Thnku for using");
+				quit=false;
+				break;
+			//if user enter wrong value
 			default:System.out.println("Please enter right key");		
-		}
+			}
 		}
 		
 	}
 		private static void printaction()
 		{
 			System.out.println("print the action\n"+
-            "1.for static max\n"+
-            "2.for static min\n"+
+            		"1.for static max\n"+
+            		"2.for static min\n"+
 			"3.for static 2d max\n"+
 			"4.for static 2d min\n"+
 			"5.for void sort for 1D array\n"+
 			"6.for void sort for 2D array\n"+
-			"7.for union\n"+
-			"8.for intersection\n"+
 			"9.for Showing the choice\n"+
 			"10.for exit the program");
 		}
-		
-		
-
-		
-		
-		
-
-	
 	}
 	
 
